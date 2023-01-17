6235691954bd4b8d4d813a3e573716573f87bf3c
ionictemplate <53865250+ionictemplate-app@users.noreply.github.com>
2023-01-17 21:33:54 +0800

    Add project files.

---
+++ whatsapp-bulk-invite-pro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whatsapp_bulk_invite_pro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
